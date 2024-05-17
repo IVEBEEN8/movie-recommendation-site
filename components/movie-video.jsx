7adcca1aed49f30a6283354754d7bcb74d942319
Heewon Kang <145734145+IVEBEEN8@users.noreply.github.com>
2024-05-17 16:22:41 -0400

    movie-video changed

--- components/movie-video.jsx
+++ components/movie-video.jsx
@@ -12,6 +12,8 @@ export default async function MovieVideo({id}) {
   const videos = await getVideos(id);
   return <div className={styles.container}>
     {videos.map((video)=>(<iframe key={video.id} src={`https://www.youtube.com/embed/${video.key}`}
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowFullScreen
     title={video.name}
     ></iframe>
     ))}
